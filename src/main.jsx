3f5889a4fe841b42deb0708c5d9324b002890659
sam.ko <sam.ko@linexsolutions.com>
2025-11-28 14:54:45 +0800

    修正 main.jsx 中的 CameraApp 引入路徑

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CameraApp from '../CameraApp.jsx';
+import CameraApp from './CameraApp.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
